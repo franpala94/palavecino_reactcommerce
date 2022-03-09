d60cdecbd7c9070f4a9d999a6e559840d9a001c0
Franco <62515156+franpala94@users.noreply.github.com>
2022-03-09 18:07:16 -0300

    Cambios estilos

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -12,23 +12,33 @@ export const ItemDetail = ({product}) => {
             </div>
             <div class="detalle__descripcion">
                 <div class="detalle__item--izq">
-                    <h6>Nuevo</h6>
+                    <div class="detalle__box--amarillo">
+                        <h6>Nuevo</h6>
+                    </div>                    
                     <h3>{product.marca} {product.modelo} {product.category}</h3>
-                    <h1>$ {product.precio}</h1>
+                    <h1 class="detalle__label--amarillo">$ {product.precio}</h1>
                     <h6>en 12x $ {cuotas.toFixed(2)}</h6>
                     <h6>Todos los medios de pago.</h6>
-                </div>   
-
-                <ItemCount stock={product.stock} initial={1}/>
-
-                <div class="detalle__item--center">
-                    <h5>Descripcion</h5>
-                    <p>{product.descripcion}</p>
+                    <div class="detalle__pagos">
+                        <i class='bx bxl-visa' ></i>
+                        <i class='bx bxl-mastercard' ></i>
+                        <i class='bx bxs-bank' ></i>
+                        <i class='bx bx-money'></i>
+                        <i class='bx bxl-paypal' ></i>                        
+                    </div>
                 </div>
                 
+                <ItemCount stock={product.stock} initial={1}/>
+                <div class="d-flex flex-row justify-content-center">
+                    <i class='bx bx-check-shield detalle__pagos' ></i>
+                    <p>Compra protegida</p>
+                </div>
+
+                <div class="detalle__item--center">
+                    <h5>Descripción</h5>
+                    <p>{product.descripcion}</p>
+                </div> 
             </div>
-            
-            
         </div>
 
     )
